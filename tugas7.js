10dcf00c2c6551ca1cd23ffb9eaed71a801ebbab
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-21 20:33:45 +0700

    Tugas 7 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+let tinggi=[165,160,180,190,175,170,162,166,182,191];
+for (var i=0; i<tinggi.length; i++){
+    console.log(tinggi[i]);
+}
+console.log("for of")
+for(let x of tinggi){
+    console.log(x);
+}
